383dc0209cecba3c67e222563ff86fb933d3196c
rabah babaci <4rabah@gmail.com>
2020-09-09 09:04:57 -0700

    implement the slowfun using a table for caching

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,26 +1,42 @@
+import random, math
+from time import time
+
 # Your code here
 
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= x + y
     v %= 982451653
 
     return v
+
+
+table = {}
+
 
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if f"{x}_{y}" not in table:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= x + y
+        v %= 982451653
+        table[f"{x}_{y}"] = v
 
+    return table[f"{x}_{y}"]
 
 
 # Do not modify below this line!
+start = time()
 
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
+
+print(f"It took {time() - start} seconds.")
