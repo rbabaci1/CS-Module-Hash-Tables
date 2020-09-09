624a53c3045d717bf068d86ca601f8be70432bcd
rabah babaci <4rabah@gmail.com>
2020-09-09 09:27:21 -0700

    implement a faster version of expensive_seq using a hash table

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,11 +1,17 @@
+table = {}
+
+
 def expensive_seq(x, y, z):
     if x <= 0:
         return y + z
-    return (
-        expensive_seq(x - 1, y + 1, z)
-        + expensive_seq(x - 2, y + 2, z * 2)
-        + expensive_seq(x - 3, y + 3, z * 3)
-    )
+
+    if (x, y, z) not in table:
+        table[(x, y, z)] = (
+            expensive_seq(x - 1, y + 1, z)
+            + expensive_seq(x - 2, y + 2, z * 2)
+            + expensive_seq(x - 3, y + 3, z * 3)
+        )
+    return table[(x, y, z)]
 
 
 if __name__ == "__main__":
@@ -13,4 +19,4 @@ if __name__ == "__main__":
         x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+    # print(expensive_seq(150, 400, 800))
