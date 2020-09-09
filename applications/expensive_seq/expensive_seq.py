6ed7e73b3011e83c4124e84893eb530ebb8fd002
rabah babaci <4rabah@gmail.com>
2020-09-09 09:28:48 -0700

    .

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
         x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    # print(expensive_seq(150, 400, 800))
+    print(expensive_seq(150, 400, 800))
