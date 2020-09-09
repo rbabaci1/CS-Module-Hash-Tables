8397cc54fef5d28790eaea77c12a57e7ce4c94d5
rabah babaci <4rabah@gmail.com>
2020-09-09 09:28:42 -0700

    .

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,8 +1,6 @@
 import random, math
 from time import time
 
-# Your code here
-
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -21,14 +19,14 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    if f"{x}_{y}" not in table:
+    if (x, y) not in table:
         v = math.pow(x, y)
         v = math.factorial(v)
         v //= x + y
         v %= 982451653
-        table[f"{x}_{y}"] = v
+        table[(x, y)] = v
 
-    return table[f"{x}_{y}"]
+    return table[(x, y)]
 
 
 # Do not modify below this line!
