cdabf0e6b48e81b2a85e276112f8f74917a5156f
rabah babaci <4rabah@gmail.com>
2020-09-11 10:30:14 -0700

    .

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,18 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+words = words.split()
+dictionary = {}
 
+for i in range(len(words) - 1):
+    word = words[i]
+    if word in dictionary:
+        dictionary[word].add(words[i + 1])
+    else:
+        dictionary[word] = set()
+        dictionary[word].add(words[i + 1])
 
+print(dictionary)
 # TODO: construct 5 random sentences
 # Your code here
 
