84eb8935e92cfc52b9838103b01c769cd062385d
rabah babaci <4rabah@gmail.com>
2020-09-11 14:22:32 -0700

    implement the draw histogram function

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,21 @@
-# Your code here
+import sys
 
+sys.path.append("../word_count")
+from word_count import word_count
+
+
+def draw_histogram(file_name):
+    with open(file_name) as f:
+        words = f.read()
+    words = word_count(words)
+    sorted_words = sorted(words.items(), key=lambda x: (-x[1], x[0]))
+    longest_word = max(len(w) for w in words)
+
+    for x in sorted_words:
+        res = " " * (longest_word - len(x[0]) + 2)
+        for _ in range(x[1]):
+            res += "#"
+        print(f"{x[0].lower()}{res}")
+
+
+draw_histogram("robin.txt")
